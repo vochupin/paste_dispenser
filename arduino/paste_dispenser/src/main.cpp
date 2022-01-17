7a2701e3b208c1ac90690787ece7c65547dd2fb2
Vladimir Chupin <vochupin@gmail.com>
2022-01-17 19:54:09 +0700

    поменял распиновку после перепайки

--- arduino/paste_dispenser/src/main.cpp
+++ arduino/paste_dispenser/src/main.cpp
@@ -4,8 +4,8 @@
 // GPIO I/O pins on the Arduino connected to strobe, clock, data,
 //pick on any I/O you want.
 #define  STROBE_TM 4 // strobe = GPIO connected to strobe line of module
-#define  CLOCK_TM 3  // clock = GPIO connected to clock line of module
-#define  DIO_TM 2 // data = GPIO connected to data line of module
+#define  CLOCK_TM 5  // clock = GPIO connected to clock line of module
+#define  DIO_TM 6 // data = GPIO connected to data line of module
 bool high_freq = false; //default false, If using a high freq CPU > ~100 MHZ set to true. 
  
 // Constructor object (GPIO STB , GPIO CLOCK , GPIO DIO, use high freq MCU)
