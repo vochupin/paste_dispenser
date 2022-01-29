2da8875a5eb73b1e5cac0d66d435b99213180431
Vladimir Chupin <vochupin@gmail.com>
2022-01-30 00:39:04 +0700

    добавил старт последовательности управления клапанами

--- arduino/paste_dispenser/src/main.cpp
+++ arduino/paste_dispenser/src/main.cpp
@@ -5,6 +5,9 @@
 #include <TM1638plus.h> //include the library
 #include "settings.h"
 
+#define RELAY_ON  1
+#define RELAY_OFF 0
+
 // STR2INT errors
 #define STR2INT_SUCCESS 0
 #define STR2INT_INCONVERTIBLE 1
@@ -16,19 +19,28 @@ typedef uint8_t str2int_errno;
 #define MST_PAUSE 2
 #define MST_ATMO_DELAY 3
 
+// Timer states
+#define TST_IDLE              0
+#define TST_PRESSURE_DELAY    1
+#define TST_PAUSE             2
+#define TST_ATMO_DELAY        3
+
 // Key definitions
-#define KEY_PRESSURE_DELAY 1
-#define KEY_PAUSE 2
-#define KEY_ATMO_DELAY 4
-#define KEY_10_5 8
-#define KEY_10_4 16
-#define KEY_10_3 32
-#define KEY_10_2 64
-#define KEY_10_1 128
+#define KEY_PRESSURE_DELAY    1
+#define KEY_PAUSE             2
+#define KEY_ATMO_DELAY        4
+#define KEY_10_5              8
+#define KEY_10_4              16
+#define KEY_10_3              32
+#define KEY_10_2              64
+#define KEY_10_1              128
 
 // Relay control pins
 #define PRESSURE_RELAY 7
 #define ATMO_RELAY 8
+
+//Start trigger input
+#define START_TRIGGER_PIN 2
 
 // GPIO I/O pins on the Arduino connected to strobe, clock, data,
 // pick on any I/O you want.
@@ -41,6 +53,7 @@ void showMenu();
 void updateSettingsFromCurrentValue();
 void changeDecimalPlace(uint8_t place);
 str2int_errno str2uint16(uint16_t *out, char *s);
+void start();
 
 // Constructor object (GPIO STB , GPIO CLOCK , GPIO DIO, use high freq MCU)
 TM1638plus tm(STROBE_TM, CLOCK_TM, DIO_TM, high_freq);
@@ -54,6 +67,15 @@ uint16_t currentMenuValue = 0;
 uint8_t menuState = MST_PRESSURE_DELAY;
 uint8_t oldBtns = 0;
 
+uint16_t cnt = 0;
+uint8_t timerState = TST_IDLE;
+int oldStartPinState;
+
+enum Prescalers {
+    prescaler0, prescaler1, prescaler8, prescaler32,
+    prescaler64,  prescaler128,  prescaler256,  prescaler1024
+};
+
 void setup() {
   pinMode(PRESSURE_RELAY, OUTPUT);
   pinMode(ATMO_RELAY, OUTPUT);
@@ -61,13 +83,57 @@ void setup() {
   digitalWrite(PRESSURE_RELAY, LOW);
   digitalWrite(ATMO_RELAY, LOW);
 
+  pinMode(START_TRIGGER_PIN, INPUT_PULLUP);
+  oldStartPinState = digitalRead(START_TRIGGER_PIN);
+
   tm.displayBegin();
 
   settings = readSettings();
+
+  cli();
+  TCCR2A = 0;
+  TCCR2B = prescaler64; //for osc=16MHz - 1mS
+  TIMSK2 |= (1<<TOIE2);
+  sei(); 
+}
+
+ISR(TIMER2_OVF_vect){
+  if (cnt != 0) {
+    cnt--;
+  } else {
+    switch(timerState) {
+      case TST_PRESSURE_DELAY:
+        digitalWrite(PRESSURE_RELAY, RELAY_OFF);
+        digitalWrite(ATMO_RELAY, RELAY_OFF);
+        timerState = TST_PAUSE;
+        cnt = settings->pauseDelay;
+        break;
+      
+      case TST_PAUSE:
+        digitalWrite(PRESSURE_RELAY, RELAY_OFF);
+        digitalWrite(ATMO_RELAY, RELAY_ON);
+        timerState = TST_ATMO_DELAY;
+        cnt = settings->atmoDelay;
+        break;
+
+      case TST_ATMO_DELAY:
+      default:
+        digitalWrite(PRESSURE_RELAY, RELAY_OFF);
+        digitalWrite(ATMO_RELAY, RELAY_OFF);
+        timerState = TST_IDLE;
+        cnt = 0;
+    }
+  }
 }
 
 void loop() {
   showMenu();
+
+  int startPin = digitalRead(START_TRIGGER_PIN);
+  if (startPin == LOW && oldStartPinState == HIGH) {
+    start();
+  }
+  oldStartPinState = startPin;
 }
 
 void showMenu() {
@@ -188,4 +254,13 @@ str2int_errno str2uint16(uint16_t *out, char *s) {
     return STR2INT_INCONVERTIBLE;
   *out = l;
   return STR2INT_SUCCESS;
+}
+
+void start() {
+  cli();
+  digitalWrite(PRESSURE_RELAY, RELAY_ON);
+  digitalWrite(ATMO_RELAY, RELAY_OFF);
+  timerState = TST_PRESSURE_DELAY;
+  cnt = settings->pressureDelay;
+  sei();
 }
