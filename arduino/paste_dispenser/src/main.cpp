9d8f798d749d5b28158ff55c767383b8e50fa381
Vladimir Chupin <vochupin@gmail.com>
2022-01-29 11:30:41 +0700

    выделил обновление текущего значения в отдельную функцию

--- arduino/paste_dispenser/src/main.cpp
+++ arduino/paste_dispenser/src/main.cpp
@@ -2,30 +2,36 @@
 #include <TM1638plus.h> //include the library
 
 void showMenu();
+void updateCurrentValue();
 
-//Menu states
-#define MST_PRESSURE_DELAY  1
-#define MST_PAUSE           2
-#define MST_ATMO_DELAY      3
+// Menu states
+#define MST_PRESSURE_DELAY 1
+#define MST_PAUSE 2
+#define MST_ATMO_DELAY 3
 
-//Key definitions
-#define KEY_PRESSURE_DELAY    1
-#define KEY_PAUSE             2
-#define KEY_ATMO_DELAY        4
+// Key definitions
+#define KEY_PRESSURE_DELAY 1
+#define KEY_PAUSE 2
+#define KEY_ATMO_DELAY 4
+#define KEY_10_5 8
+#define KEY_10_4 16
+#define KEY_10_3 32
+#define KEY_10_2 64
+#define KEY_10_1 128
 
-//Relay control pins
-#define PRESSURE_RELAY  7
-#define ATMO_RELAY      8
+// Relay control pins
+#define PRESSURE_RELAY 7
+#define ATMO_RELAY 8
 
 // GPIO I/O pins on the Arduino connected to strobe, clock, data,
-//pick on any I/O you want.
-#define  STROBE_TM 4 // strobe = GPIO connected to strobe line of module
-#define  CLOCK_TM 5  // clock = GPIO connected to clock line of module
-#define  DIO_TM 6 // data = GPIO connected to data line of module
-bool high_freq = false; //default false, If using a high freq CPU > ~100 MHZ set to true. 
- 
+// pick on any I/O you want.
+#define STROBE_TM 4     // strobe = GPIO connected to strobe line of module
+#define CLOCK_TM 5      // clock = GPIO connected to clock line of module
+#define DIO_TM 6        // data = GPIO connected to data line of module
+bool high_freq = false; // default false, If using a high freq CPU > ~100 MHZ set to true.
+
 // Constructor object (GPIO STB , GPIO CLOCK , GPIO DIO, use high freq MCU)
-TM1638plus tm(STROBE_TM, CLOCK_TM , DIO_TM, high_freq);
+TM1638plus tm(STROBE_TM, CLOCK_TM, DIO_TM, high_freq);
 
 char buffer[40];
 
@@ -33,7 +39,10 @@ uint16_t pressureDelay = 50;
 uint16_t pauseDelay = 10;
 uint16_t atmoDelay = 50;
 
+uint16_t currentMenuValue = 0;
+
 uint8_t menuState = MST_PRESSURE_DELAY;
+uint8_t oldBtns = 0;
 
 void setup() {
   pinMode(PRESSURE_RELAY, OUTPUT);
@@ -42,7 +51,7 @@ void setup() {
   digitalWrite(PRESSURE_RELAY, LOW);
   digitalWrite(ATMO_RELAY, LOW);
 
-  tm.displayBegin(); 
+  tm.displayBegin();
 }
 
 void loop() {
@@ -62,21 +71,9 @@ void loop() {
 }
 
 void showMenu() {
-  uint8_t btns = tm.readButtons();
+  const char *menuPrefix = "???";
 
-  if (btns & KEY_PRESSURE_DELAY) {
-    menuState = MST_PRESSURE_DELAY;
-  } else if (btns & KEY_PAUSE) {
-    menuState = MST_PAUSE;
-  } else if (btns & KEY_ATMO_DELAY) {
-    menuState = MST_ATMO_DELAY;
-  }
-
-  uint16_t currentMenuValue = 0;
-
-  const char* menuPrefix = "???";
-  switch (menuState)
-  {
+  switch (menuState) {
   case MST_PRESSURE_DELAY:
     menuPrefix = "Pd ";
     currentMenuValue = pressureDelay;
@@ -91,9 +88,42 @@ void showMenu() {
     break;
   }
 
-  sprintf(buffer, "%s%05d", menuPrefix, currentMenuValue);
+  uint8_t btns = tm.readButtons();
 
+  if (oldBtns == 0) {
+    if (btns & KEY_PRESSURE_DELAY) {
+      menuState = MST_PRESSURE_DELAY;
+      currentMenuValue = pressureDelay;
+    } else if (btns & KEY_PAUSE) {
+      menuState = MST_PAUSE;
+      currentMenuValue = pauseDelay;
+    } else if (btns & KEY_ATMO_DELAY) {
+      menuState = MST_ATMO_DELAY;
+      currentMenuValue = atmoDelay;
+    } else if (btns & KEY_10_5) {
+      currentMenuValue += 10000;
+      updateCurrentValue();
+    }
+  }
+
+  oldBtns = btns;
+
+  sprintf(buffer, "%s%05d", menuPrefix, currentMenuValue);
   tm.displayText(buffer);
-  
+
   delay(100);
+}
+
+void updateCurrentValue() {
+    switch (menuState) {
+  case MST_PRESSURE_DELAY:
+    pressureDelay = currentMenuValue;
+    break;
+  case MST_PAUSE:
+    pauseDelay = currentMenuValue;
+    break;
+  case MST_ATMO_DELAY:
+    atmoDelay = currentMenuValue;
+    break;
+  }
 }
