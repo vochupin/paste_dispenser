a8370c1cc959047a60abdae65a2d3b47ae9c8f52
Vladimir Chupin <vochupin@gmail.com>
2022-01-29 12:22:11 +0700

    редактируем цифру посимвольно

--- arduino/paste_dispenser/src/main.cpp
+++ arduino/paste_dispenser/src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
 #include <TM1638plus.h> //include the library
 
-void showMenu();
-void updateCurrentValue();
+//STR2INT errors
+#define STR2INT_SUCCESS         0
+#define STR2INT_INCONVERTIBLE   1
+#define STR2INT_OVERFLOW        2
+typedef uint8_t str2int_errno;
 
 // Menu states
 #define MST_PRESSURE_DELAY 1
@@ -29,6 +35,11 @@ void updateCurrentValue();
 #define CLOCK_TM 5      // clock = GPIO connected to clock line of module
 #define DIO_TM 6        // data = GPIO connected to data line of module
 bool high_freq = false; // default false, If using a high freq CPU > ~100 MHZ set to true.
+
+void showMenu();
+void updateFromCurrentValue();
+void changeDecimalPlace(uint8_t place);
+str2int_errno str2uint16(uint16_t *out, char *s);
 
 // Constructor object (GPIO STB , GPIO CLOCK , GPIO DIO, use high freq MCU)
 TM1638plus tm(STROBE_TM, CLOCK_TM, DIO_TM, high_freq);
@@ -101,21 +112,52 @@ void showMenu() {
       menuState = MST_ATMO_DELAY;
       currentMenuValue = atmoDelay;
     } else if (btns & KEY_10_5) {
-      currentMenuValue += 10000;
-      updateCurrentValue();
+      changeDecimalPlace(5);
+      updateFromCurrentValue();
+    } else if (btns & KEY_10_4) {
+      changeDecimalPlace(4);
+      updateFromCurrentValue();
+    } else if (btns & KEY_10_3) {
+      changeDecimalPlace(3);
+      updateFromCurrentValue();
+    } else if (btns & KEY_10_2) {
+      changeDecimalPlace(2);
+      updateFromCurrentValue();
+    } else if (btns & KEY_10_1) {
+      changeDecimalPlace(1);
+      updateFromCurrentValue();
     }
   }
 
   oldBtns = btns;
 
-  sprintf(buffer, "%s%05d", menuPrefix, currentMenuValue);
+  sprintf(buffer, "%s%05u", menuPrefix, currentMenuValue);
   tm.displayText(buffer);
 
   delay(100);
 }
 
-void updateCurrentValue() {
-    switch (menuState) {
+void changeDecimalPlace(uint8_t place) {
+  sprintf(buffer, "%05u", currentMenuValue);
+  
+  char symbol = buffer[5 - place];
+
+  if (symbol == '9') {
+    symbol = '0';
+  } else {
+    symbol++;
+  }
+
+  buffer[5 - place] = symbol;
+
+  uint16_t out = 0;
+  if (str2uint16(&out, buffer) == STR2INT_SUCCESS) {
+    currentMenuValue = out;
+  }
+}
+
+void updateFromCurrentValue() {
+  switch (menuState) {
   case MST_PRESSURE_DELAY:
     pressureDelay = currentMenuValue;
     break;
@@ -126,4 +168,18 @@ void updateCurrentValue() {
     atmoDelay = currentMenuValue;
     break;
   }
+}
+
+str2int_errno str2uint16(uint16_t *out, char *s) {
+    char *end;
+    if (s[0] == '\0' || isspace(s[0]))
+        return STR2INT_INCONVERTIBLE;
+    errno = 0;
+    long l = strtol(s, &end, 10);
+    if (l > UINT_MAX || (errno == ERANGE && l == LONG_MAX))
+        return STR2INT_OVERFLOW;
+    if (*end != '\0')
+        return STR2INT_INCONVERTIBLE;
+    *out = l;
+    return STR2INT_SUCCESS;
 }
